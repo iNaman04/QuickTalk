a94c31dc26ad9468a66c72d7940fbc63ce5d492c
Namandeep Singh <naman25singh@gmail.com>
2025-10-22 22:30:43 +0530

    basic layout

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth_route.js';
+import { connectDB } from './lib/db.js';
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;  
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log("Server is running on PORT " + PORT);
+  connectDB();
+});
